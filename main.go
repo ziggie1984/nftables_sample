92be70246168bff5d3f4ccf0027c307aee4584be
ziggie <ziggie1984@protonmail.com>
2022-06-03 23:17:26 +0200

    list chains

--- main.go
+++ main.go
@@ -2,170 +2,79 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"os"
 
 	"github.com/google/nftables"
-	"github.com/google/nftables/expr"
 )
 
 func main() {
 
-	// // option := nftables.AsLasting()
+	// option := nftables.AsLasting()
 
-	// // clientNFT, error := nftables.New(option)
+	// clientNFT, error := nftables.New(option)
 
-	// clientNFT := &nftables.Conn{}
+	clientNFT := &nftables.Conn{}
 
-	// defer clientNFT.CloseLasting()
+	defer clientNFT.CloseLasting()
 
-	// // if error != nil {
-	// // 	fmt.Println("Error Initializing nftables", error)
-	// // 	os.Exit(1)
-	// // } else {
-	// // 	fmt.Println("nftables initialized")
-	// // 	fmt.Printf("Connection %v, %v\n", clientNFT.NetNS, clientNFT.TestDial)
-
-	// // }
-
-	// wgTable := &nftables.Table{
-	// 	Name:   "testtable",
-	// 	Family: nftables.TableFamilyINet,
-	// }
-	// clientNFT.AddTable(wgTable)
-
-	// prerouting := &nftables.Chain{
-	// 	Name:     "testchain",
-	// 	Table:    wgTable,
-	// 	Hooknum:  nftables.ChainHookPrerouting,
-	// 	Priority: nftables.ChainPriorityNATDest,
-	// 	Type:     nftables.ChainTypeNAT,
-	// }
-	// fmt.Printf("Chain Self-Created: %v\n", prerouting)
-	// prerouting = clientNFT.AddChain(prerouting)
-
-	// tables, error := clientNFT.ListTables()
 	// if error != nil {
-	// 	fmt.Println("Error Getting Chains", error)
+	// 	fmt.Println("Error Initializing nftables", error)
 	// 	os.Exit(1)
-	// }
-
-	// for _, value := range tables {
-	// 	fmt.Printf("Table: %v\n", value)
-	// }
-
-	// chains, error := clientNFT.ListChains()
-	// if error != nil {
-	// 	fmt.Println("Error Getting Chains", error)
-	// 	os.Exit(1)
-	// }
-
-	// for _, value := range chains {
-	// 	fmt.Printf("Chain: %v\n", value)
-	// }
-
-	// // rule := nftables.Rule{
-	// // 	Table: &wgTable,
-	// // 	Chain: &prerouting,
-	// // 	// The list of possible flags are specified by nftnl_rule_attr, see
-	// // 	// https://git.netfilter.org/libnftnl/tree/include/libnftnl/rule.h#n21
-	// // 	// Current nftables go implementation supports only
-	// // 	// NFTNL_RULE_POSITION flag for setting rule at position 0
-	// // 	Exprs: []expr.Any{},
-	// // }
-	// // clientNFT.AddRule(&rule)
-
-	// set := &nftables.Set{
-	// 	Name:    "whitelist",
-	// 	Table:   wgTable,
-	// 	KeyType: nftables.TypeIPAddr, // our keys are IPv4 addresses
-	// }
-
-	// clientNFT.AddRule(&nftables.Rule{
-	// 	Table: wgTable,
-	// 	Chain: prerouting,
-	// 	Exprs: []expr.Any{
-	// 		// [ payload load 4b @ network header + 16 => reg 1 ]
-	// 		&expr.Payload{
-	// 			DestRegister: 1,
-	// 			Base:         expr.PayloadBaseNetworkHeader,
-	// 			Offset:       16,
-	// 			Len:          4,
-	// 		},
-	// 		// [ lookup reg 1 set whitelist ]
-	// 		&expr.Lookup{
-	// 			SourceRegister: 1,
-	// 			SetName:        set.Name,
-	// 			SetID:          set.ID,
-	// 		},
-	// 		//[ immediate reg 0 drop ]
-	// 		&expr.Verdict{
-	// 			Kind: expr.VerdictDrop,
-	// 		},
-	// 	},
-	// })
-
-	// clientNFT.Flush()
+	// } else {
+	// 	fmt.Println("nftables initialized")
+	// 	fmt.Printf("Connection %v, %v\n", clientNFT.NetNS, clientNFT.TestDial)
 
 	// }
-	c := &nftables.Conn{}
 
-	// Basic boilerplate; create a table & chain.
-	table := &nftables.Table{
-		Family: nftables.TableFamilyIPv4,
-		Name:   "ip_filter",
+	wgTable := &nftables.Table{
+		Name:   "testtable",
+		Family: nftables.TableFamilyINet,
 	}
-	table = c.AddTable(table)
+	clientNFT.AddTable(wgTable)
 
-	myChain := c.AddChain(&nftables.Chain{
-		Name:     "filter_chain",
-		Table:    table,
-		Type:     nftables.ChainTypeFilter,
-		Hooknum:  nftables.ChainHookInput,
-		Priority: nftables.ChainPriorityFilter,
-	})
+	prerouting := &nftables.Chain{
+		Name:     "testchain",
+		Table:    wgTable,
+		Hooknum:  nftables.ChainHookPrerouting,
+		Priority: nftables.ChainPriorityNATDest,
+		Type:     nftables.ChainTypeNAT,
+	}
+	fmt.Printf("Chain Self-Created: %v\n", prerouting)
+	prerouting = clientNFT.AddChain(prerouting)
 
-	set := &nftables.Set{
-		Name:    "whitelist",
-		Table:   table,
-		KeyType: nftables.TypeIPAddr, // our keys are IPv4 addresses
+	clientNFT.Flush()
+
+	tables, error := clientNFT.ListTables()
+	if error != nil {
+		fmt.Println("Error Getting Chains", error)
+		os.Exit(1)
 	}
 
-	// Create the set with a bunch of initial values.
-	if err := c.AddSet(set, []nftables.SetElement{
-		{Key: net.ParseIP("8.8.8.8")},
-	}); err != nil {
-		// handle error
+	for _, value := range tables {
+		fmt.Printf("Table: %v\n", value)
 	}
 
-	c.AddRule(&nftables.Rule{
-		Table: table,
-		Chain: myChain,
-		Exprs: []expr.Any{
-			// [ payload load 4b @ network header + 16 => reg 1 ]
-			&expr.Payload{
-				DestRegister: 1,
-				Base:         expr.PayloadBaseNetworkHeader,
-				Offset:       16,
-				Len:          4,
-			},
-			// [ lookup reg 1 set whitelist ]
-			&expr.Lookup{
-				SourceRegister: 1,
-				SetName:        set.Name,
-				SetID:          set.ID,
-			},
-			//[ immediate reg 0 drop ]
-			&expr.Verdict{
-				Kind: expr.VerdictDrop,
-			},
-		},
-	})
-	if err := c.Flush(); err != nil {
-		// handle error
-		fmt.Println("Error Flushing Ruleset", err)
-	} else {
-		fmt.Println("Flushing Ruleset was successful")
-
+	chains, error := clientNFT.ListChains()
+	if error != nil {
+		fmt.Println("Error Getting Chains", error)
+		os.Exit(1)
 	}
+
+	for _, value := range chains {
+		fmt.Printf("Chain: %v\n", value)
+	}
+
+	// rule := nftables.Rule{
+	// 	Table: &wgTable,
+	// 	Chain: &prerouting,
+	// 	// The list of possible flags are specified by nftnl_rule_attr, see
+	// 	// https://git.netfilter.org/libnftnl/tree/include/libnftnl/rule.h#n21
+	// 	// Current nftables go implementation supports only
+	// 	// NFTNL_RULE_POSITION flag for setting rule at position 0
+	// 	Exprs: []expr.Any{},
+	// }
+	// clientNFT.AddRule(&rule)
+
+	clientNFT.Flush()
 
 }
